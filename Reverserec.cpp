73b558b1e7d142b2c41b1bb60ec9b0f5b0cf6011
Roshni-Varma <84094325+Roshni-Varma@users.noreply.github.com>
2021-06-07 13:51:48 +0530

    Create Reverserec.cpp

---
+++ Reverserec.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void arrayrev(int l,int r,int a[])
+{
+	if(l>=r)
+	{
+		return;
+	}
+	swap(a[l],a[r]);
+	return arrayrev(l+1,r-1,a);
+	}
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	arrayrev(0,n-1,a);
+	return 0;
+}
